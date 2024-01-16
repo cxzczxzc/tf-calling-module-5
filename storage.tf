20274d22c305c338cb3846945315b025e0a82157
Saad Ahmad <saadx@google.com>
2024-01-16 10:50:34 -0500

    updated base module link

--- storage.tf
+++ storage.tf
@@ -1,5 +1,5 @@
 module "terraform_test_bucket" {
-  source          = "github.com/cxzczxzc/base-modules/tree/main/base-modules/terraform-google-cloud-storage-master-5" #change to gcs before deploy
+  source          = "github.com/cxzczxzc/tf-base-module-5" #change to gcs before deploy
   project_id      = var.project_id
   prefix          = "storage"
   names           = ["jakaiti-ex-bkt-5"]
